d2fb8d8cfc17fc8d694582982c7562ec7068dc04
Wally Jniyah <ojniyah@gmail.com>
2017-11-06 02:23:23 -0500

    fixed the footer and reconstructed my javascript

--- javascript/horoscopes.js
+++ javascript/horoscopes.js
@@ -2,38 +2,34 @@
 var quote;
 var icon;
 
-
-
-   /* API request */
-   function updateByChoice() {
-     var input1 = $("#search1").val();
-    var url = "http://sandipbgt.com/theastrologer/api/horoscope/" + input1 + "/today/?";
-        sendRequest(url);
-
-   }
-
-
-
-   window.onload = function () {
+// the windows onload function
+      window.onload = function () {
           quote = document.getElementById('quote');
           icon = document.getElementById('icon');
+          var search1 = document.querySelector("#search1");
      };
 
+     /* API request */
+     function updateByChoice() {
+       var input1 = $("#search1").val();
+      var url = "http://sandipbgt.com/theastrologer/api/horoscope/" + input1 + "/today/?";
+          sendRequest(url);
+          }
 
-
-   /* inner the weather API  json information into the variable declared above */
-   function update(zodiac) {
-     quote.innerHTML = zodiac.quote;
-     icon.innerHTML =  '<img height="300" src = "img/' + zodiac.sign + '.png">';
-   }
-
-/* ----AJAX request using jQuery---- */
-function sendRequest(url) {
-  $.getJSON( url, function adinfo (data) {
-
+   /* ----AJAX request to the API using jQuery ---- */
+          function sendRequest(url) {
+            $.getJSON( url, function adinfo (data) {
                   var zodiac = {};
                   zodiac.quote = data.horoscope;
-                  zodiac.sign = data.sunsign.toLowerCase();;
+                  zodiac.sign = data.sunsign.toLowerCase();
                   update(zodiac);
               });
           }
+
+   /* inner the weather API  jsonP callback information into the DOM */
+   function update(zodiac) {
+     quote.innerHTML = zodiac.quote;
+     icon.innerHTML =  '<img height="300" src = "img/' + zodiac.sign + '.png">';
+     var search1 = document.querySelector("#search1");
+     search1.value = "";
+   }
